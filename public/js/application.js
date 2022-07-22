c60a106bce067c4828b2f70f3e301672750ab6fc
Vitaliy <vetal9305@gmail.com>
2022-07-22 15:58:40 +0300

    dsd

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,18 @@
+const arrAnswers = [];
+// const { cards } = require('../../db/models');
+
 document.querySelector('#mainid').addEventListener('click', async (event) => {
   event.preventDefault();
-  const number = event.path.;
-  console.log('!!!!!!!', number);
+  console.log(event.target);
+  const number = event.srcElement.id;
+  // arrAnswers.push(number);
+  
+  // const responce = await fetch('/input/:id'{
+  //   method: 'get',
+  //     headers: { 'Content-Type': 'Application/json' },
+  //     body: JSON.stringify({ number }),
+  // });
+  // console.log('!!!!!!!',responce);
   // const responce = await fetch('/rolls', {
   //   method: 'post',
   //   headers: { 'Content-Type': 'Application/json' },
--- routes/main.route.js
+++ routes/main.route.js
@@ -1,6 +1,4 @@
 const router = require('express').Router();
-const ReactDOMServer = require('react-dom/server');
-const React = require('react');
 const { decks, deckscards, cards } = require('../db/models');
 const Main = require('../views/Main');
 const First = require('../views/First');
