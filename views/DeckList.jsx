13028239c50f6440dc0a7f8e2ea209a41f0a98a7
Vitaliy <vetal9305@gmail.com>
2022-07-22 12:00:47 +0300

    next

--- views/DeckList.jsx
+++ views/DeckList.jsx
@@ -2,14 +2,14 @@ const React = require('react');
 
 function DeckList({ deck }) {
   return (
-      <div className="card" style={{ width: '18rem' }}>
-        <div className="card-body">
-          <h5 className="card-title">Тема {deck}</h5>
-          <form action="/input1" method="post">
-          <button className="btn btn-primary 2">Выбираем {deck}</button>
-          </form>
-        </div>
+    <div className="card" id={`${deck}`} style={{ width: '18rem' }}>
+      <div className="card-body">
+        <h5 className="card-title">{deck}</h5>
+        <form action="/input1" method="post">
+          <button className="btn btn-primary 2">Выбираем</button>
+        </form>
       </div>
+    </div>
   );
 }
 
--- views/Main.jsx
+++ views/Main.jsx
@@ -1,14 +1,13 @@
 const React = require('react');
 const Layout = require('./Layout');
 const DeckList = require('./DeckList');
-const arr =[1,2,3,4,5]
 
-function Main({ title }) {
+function Main({ title, deck }) {
   return (
     <Layout title={title}>
-      <section className = "container">
-      <h1>Заголовок</h1>
-        {arr.map(el => <DeckList key={el} deck = {el}/> )}
+      <section className="container">
+        <h1>Заголовок</h1>
+        {deck.map((el) => <DeckList key={el.id} deck={el.deck} />)}
       </section>
     </Layout>
   );
