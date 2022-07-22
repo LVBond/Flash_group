9d1688870e126873523bcd9a90f9422930dc437c
Vitaliy <vetal9305@gmail.com>
2022-07-22 10:16:33 +0300

    fix

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
-const { decks, cards } = require('../db/models');
+const ReactDOMServer = require('react-dom/server');
+const React = require('react');
+const Main = require('../views/Main');
+// const { decks, cards } = require('../db/models');
 
 router.get('/', async (req, res) => {
-  const deck = await decks.findAll({ raw: true, attributes: ['deck'] });
-  const card = await cards.findAll({ raw: true, attributes: ['card'] });
-  const result = deck.map((el) => el.deck);
-  const resultCard = card.map((el) => el.card);
-  console.log(result);
-  console.log(resultCard);
-  res.send(resultCard);
+  // const deck = await decks.findAll({ raw: true, attributes: ['deck'] });
+  // const card = await cards.findAll({ raw: true, attributes: ['card'] });
+  // const result = deck.map((el) => el.deck);
+  // const resultCard = card.map((el) => el.card);
+  // console.log(result);
+  // console.log(resultCard);
+  // res.send(resultCard);
+  const main = React.createElement(Main, { title: 'Express' });
+  const html = ReactDOMServer.renderToStaticMarkup(main);
+  res.write('<!DOCTYPE html>');
+  res.end(html);
 });
 
 module.exports = router;
