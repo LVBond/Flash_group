774f3c466f04c5f91888f51b3e7ad86ddc3c5a74
Vitaliy <vetal9305@gmail.com>
2022-07-22 16:34:11 +0300

    qwe

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 require('@babel/register');
-
 const express = require('express');
-
-
-
-
-// const { sequelize } = require('./db/models');
 const mainRoute = require('./routes/main.route');
 
 const app = express();
 const config = require('./config/config');
-// Полключение реакта и реактДом:
 
 const PORT = process.env.PORT ?? 3000;
 config(app);
@@ -20,7 +13,6 @@ app.use('/', mainRoute);
 app.listen(PORT, async () => {
   try {
     console.log(`Server started at ${PORT} port`);
-    // await sequelize.authenticate();
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
